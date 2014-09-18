576fc22c8fda65525a7301e63b5b33f5aa55b1b8
joshuaadampoirier <josh.poirier@gmail.com>
2014-09-18 01:52:22 -0600

    Parts 1-3 working, essence of part 4 is working

--- run_analysis.r
+++ run_analysis.r
@@ -20,7 +20,7 @@ run_analysis <- function() {
     
     ## Read the data - add 'Status' column indicating training or test data
     ##      NOTE:  Assume features.txt is clean and does not require modifications
-    columnNames <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)[,2]
+    columnNames <- cleanColumns()
     trainingData <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = columnNames)
     testData <- read.table("./UCI HAR Dataset/test/X_test.txt", col.names = columnNames)
     trainingData$Status = "Training"
@@ -31,19 +31,35 @@ run_analysis <- function() {
     rm(columnNames, trainingData, testData)     # cleanup memory
     
     ## ** PART TWO ** Extract only the measurements on the mean and standard deviation
-    data <- select(data, contains("mean()"), contains("std()")) 
+    data <- select(data, contains("mean"), contains("std"), -contains("meanFreq"), -contains("angle"), -contains("gravityMean")) 
     
     ## ** PART THREE ** Create descriptive activity variable column by linking the 'X' data files with
     ##                  the 'Y' activity data and 'activity labels' files
-    ##      NOTE:   This part utilizes the getActivityData() function found below
+    ##      NOTE:   1)  This part utilizes the getActivityData() function found below
+    ##              2)  We will also add a "subject" variable based on subject_test.txt and 
+    ##                  subject_train.txt
     trainingAct <- getActivityData("./UCI HAR Dataset/train/y_train.txt")
     testAct <- getActivityData("./UCI HAR Dataset/test/y_test.txt")
-    ActivityData <- rbind_list(trainingAct, testAct)$ActivityLabel
-    data <- cbind(ActivityData, data)
+    ActivityLabel <- rbind_list(trainingAct, testAct)$ActivityLabel
+    trainingSubjects <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "SubjectKey")
+    testSubjects <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "SubjectKey")
+    SubjectKey <- rbind_list(trainingSubjects, testSubjects)
+    data <- cbind(ActivityLabel, SubjectKey, data)
     
     ## ** PART FOUR **
+    data$obsNum <- seq_len(nrow(data))
+    data <- gather(data, variable, statisticValue, -ActivityLabel, -SubjectKey, -obsNum, -contains("angle"))
+    data <- separate(data, col = variable, into = c("Domain", "variable"), sep = 1)
+    data <- separate(data, col = variable, into = c("measurement", "statistic", "component"))
+    data <- spread(data, statistic, statisticValue)
+    
+    ## TO DO:
+        ## Convert components which are string "NA" to actual NA's
+        ## To factors: ActivityLabel, SubjectKey, obsNum, Domain, measurement, component
     
     ## ** PART FIVE **
+    
+    data
 }
 
 ## ####################################################################################################
@@ -84,5 +100,31 @@ getActivityData <- function(fname) {
     
     ## Get activity label from 'activities' data frame
     activityData <- inner_join(activityData, activities, by = "ActivityKey")
+    
+}
+
+cleanColumns <- function() {
+    
+    columnNames <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)[,2]
+    columnNames <- gsub("()", "", columnNames, fixed = TRUE)
+    
+    for(i in 1:length(columnNames)) {
+        if (substring(columnNames[i], nchar(columnNames[i]) - 3) == "mean") {
+            columnNames[i] <- paste(columnNames[i], "-NA")
+        }
+        else if (substring(columnNames[i], nchar(columnNames[i]) - 2) == "std") {
+            columnNames[i] <- paste(columnNames[i], "-NA")
+        }
+    }
+    
+    columnNames <- gsub("BodyBody", "Body", columnNames)
+    columnNames <- gsub("mean-x", "x-mean", columnNames)
+    columnNames <- gsub("std-x", "x-std", columnNames)
+    columnNames <- gsub("mean-y", "y-mean", columnNames)
+    columnNames <- gsub("std-y", "y-std", columnNames)
+    columnNames <- gsub("mean-z", "z-mean", columnNames)
+    columnNames <- gsub("std-z", "z-std", columnNames)
+    
+    columnNames
     
 }
